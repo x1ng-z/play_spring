76bcfc2e7f7f991b8daf75e0e89bb82efcc6ac09
x1ng <x1ng@users.noreply.github.com>
2019-10-12 11:25:08 +0800

    init project

---
+++ src/hs/ss/test_1.java
@@ -0,0 +1,13 @@
+package hs.ss;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.lang.reflect.Constructor;
+
+public class test_1 {
+    //ServletContext servletContext;
+    public static void main(String[] args) {
+        new ClassPathXmlApplicationContext();
+    }
+
+}
