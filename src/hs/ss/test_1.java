62571379686d41785c3e4b36b13f6651f48a6ccf
x1ng <x1ng@users.noreply.github.com>
2019-10-14 12:56:46 +0800

    add applicationContext.xml

--- src/hs/ss/test_1.java
+++ src/hs/ss/test_1.java
@@ -1,13 +1,18 @@
 package hs.ss;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.lang.reflect.Constructor;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class test_1 {
     //ServletContext servletContext;
     public static void main(String[] args) {
-        new ClassPathXmlApplicationContext();
+        ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+        AtomicBoolean d=new AtomicBoolean(false);
+        d.lazySet(true);
+        d.getAndSet(true);
     }
 
 }
